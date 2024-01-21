820e62a82148b4a6a1a17e8107ce60e57e048c0b
huynq10 <155464252+huynq10@users.noreply.github.com>
2024-01-21 18:37:34 +0700

    ✨Improve solution "Medium_0198_House_Robber" problem

--- leetcode/LeetCodeSolution/LeetCodeSolution/Medium/Medium_0198_House_Robber.swift
+++ leetcode/LeetCodeSolution/LeetCodeSolution/Medium/Medium_0198_House_Robber.swift
@@ -9,21 +9,14 @@ import Foundation
 
 class Medium_0198_House_Robber {
     func rob(_ nums: [Int]) -> Int {
-        if nums.count < 3 {
-            return max(nums.first ?? 0, nums.last ?? 0)
+        var rob1 = 0
+        var rob2 = 0
+        
+        for i in nums {
+            let temp = max(i + rob1, rob2)
+            rob1 = rob2
+            rob2 = temp
         }
-        
-        var result = Array(repeating: 0, count: nums.count)
-        result[0] = nums[0]
-        result[1] = nums[1]
-        result[2] = result[0] + nums[2]
-        
-        for i in 3..<nums.count {
-            let element = nums[i]
-            result[i] = max(result[i-3], result[i-2]) + element
-        }
-        
-        let sum = max(result[nums.count - 1], result[nums.count - 2])
-        return sum
+        return rob2
     }
 }
