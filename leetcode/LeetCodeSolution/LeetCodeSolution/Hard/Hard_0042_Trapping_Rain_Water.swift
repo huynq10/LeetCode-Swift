e274d6de72adb76953eaa714fa54439efa4f08fe
huynq10 <155464252+huynq10@users.noreply.github.com>
2024-02-12 15:06:58 +0700

    📝Add "Hard_0042_Trapping_Rain_Water" document

--- leetcode/LeetCodeSolution/LeetCodeSolution/Hard/Hard_0042_Trapping_Rain_Water.swift
+++ leetcode/LeetCodeSolution/LeetCodeSolution/Hard/Hard_0042_Trapping_Rain_Water.swift
@@ -1,9 +1,22 @@
-//
-//  Hard_0042_Trapping_Rain_Water.swift
-//  LeetCodeSolution
-//
-//  Created by LAP14556 on 12/02/2024.
-//
+/*
+ https://leetcode.com/problems/trapping-rain-water/description/
+
+ Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it can trap after raining.
+
+ Example 1:
+    Input: height = [0,1,0,2,1,0,1,3,2,1,2,1]
+    Output: 6
+    Explanation: The above elevation map (black section) is represented by array [0,1,0,2,1,0,1,3,2,1,2,1]. In this case, 6 units of rain water (blue section) are being trapped.
+ 
+ Example 2:
+    Input: height = [4,2,0,3,2,5]
+    Output: 9
+  
+ Constraints:
+    n == height.length
+    1 <= n <= 2 * 104
+    0 <= height[i] <= 105
+ */
 
 import Foundation
 
