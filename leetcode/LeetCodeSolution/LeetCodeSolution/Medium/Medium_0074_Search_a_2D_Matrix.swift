be1dd5fb5c5a492de2aca36c355df142e6ae3c12
huynq10 <155464252+huynq10@users.noreply.github.com>
2024-02-12 21:13:47 +0700

    📝Add "Medium_0074_Search_a_2D_Matrix" document

--- leetcode/LeetCodeSolution/LeetCodeSolution/Medium/Medium_0074_Search_a_2D_Matrix.swift
+++ leetcode/LeetCodeSolution/LeetCodeSolution/Medium/Medium_0074_Search_a_2D_Matrix.swift
@@ -1,9 +1,26 @@
-//
-//  Medium_0074_Search_a_2D_Matrix.swift
-//  LeetCodeSolution
-//
-//  Created by LAP14556 on 12/02/2024.
-//
+/*
+ https://leetcode.com/problems/search-a-2d-matrix/description/
+ 
+ You are given an m x n integer matrix matrix with the following two properties:
+    - Each row is sorted in non-decreasing order.
+    - The first integer of each row is greater than the last integer of the previous row.
+ Given an integer target, return true if target is in matrix or false otherwise.
+ You must write a solution in O(log(m * n)) time complexity.
+
+ Example 1:
+    Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
+    Output: true
+ 
+ Example 2:
+    Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 13
+    Output: false
+ 
+ Constraints:
+    m == matrix.length
+    n == matrix[i].length
+    1 <= m, n <= 100
+    -104 <= matrix[i][j], target <= 104
+ */
 
 import Foundation
 
