d3e790bbc8b506bd901443811109e304116ac348
huynq10 <155464252+huynq10@users.noreply.github.com>
2024-02-16 23:52:50 +0700

    📝Add "Medium_1481_Least_Number_of_Unique_Integers_after_K_Removals.swift" document

--- leetcode/LeetCodeSolution/LeetCodeSolution/Medium/Medium_1481_Least_Number_of_Unique_Integers_after_K_Removals.swift
+++ leetcode/LeetCodeSolution/LeetCodeSolution/Medium/Medium_1481_Least_Number_of_Unique_Integers_after_K_Removals.swift
@@ -1,10 +1,24 @@
-//
-//  Medium_1481_Least_Number_of_Unique_Integers_after_K_Removals.swift
-//  LeetCodeSolution
-//
-//  Created by LAP14556 on 16/02/2024.
-//
+/*
+ https://leetcode.com/problems/least-number-of-unique-integers-after-k-removals/description/
+ 1481. Least Number of Unique Integers after K Removals
+ 
+ Given an array of integers arr and an integer k. Find the least number of unique integers after removing exactly k elements.
 
+ Example 1:
+    Input: arr = [5,5,4], k = 1
+    Output: 1
+    Explanation: Remove the single 4, only 5 is left.
+ 
+ Example 2:
+    Input: arr = [4,3,1,1,3,3,2], k = 3
+    Output: 2
+    Explanation: Remove 4, 2 and either one of the two 1s or three 3s. 1 and 3 will be left.
+  
+ Constraints:
+    1 <= arr.length <= 10^5
+    1 <= arr[i] <= 10^9
+    0 <= k <= arr.length
+ */
 import Foundation
 
 class Medium_1481_Least_Number_of_Unique_Integers_after_K_Removals {
