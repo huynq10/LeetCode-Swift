b78e671677f66e6ded650671cb001cb4834e779e
huynq10 <155464252+huynq10@users.noreply.github.com>
2024-02-19 22:54:48 +0700

    📝Add "Easy_0231_Power_of_Two" document

--- leetcode/LeetCodeSolution/LeetCodeSolution/Easy/Easy_0231_Power_of_Two.swift
+++ leetcode/LeetCodeSolution/LeetCodeSolution/Easy/Easy_0231_Power_of_Two.swift
@@ -1,9 +1,30 @@
-//
-//  Easy_0231_Power_of_Two.swift
-//  LeetCodeSolution
-//
-//  Created by LAP14556 on 19/02/2024.
-//
+/*
+ https://leetcode.com/problems/power-of-two/description/
+ 231. Power of Two
+ 
+ Given an integer n, return true if it is a power of two. Otherwise, return false.
+
+ An integer n is a power of two, if there exists an integer x such that n == 2x.
+
+ Example 1:
+    Input: n = 1
+    Output: true
+    Explanation: 20 = 1
+ 
+ Example 2:
+    Input: n = 16
+    Output: true
+    Explanation: 24 = 16
+ 
+ Example 3:
+    Input: n = 3
+    Output: false
+  
+ Constraints:
+ -231 <= n <= 231 - 1
+
+ Follow up: Could you solve it without loops/recursion?
+ */
 
 import Foundation
 
